f4378a2bfce19f90e2e144a50e2ec2a8a3492db7
KozimHaydarov <kozimxaydarov208@gmail.com>
2024-08-30 15:17:30 +0300

    lesoon 004

---
+++ app.js
@@ -0,0 +1,161 @@
+
+// 1-misol
+
+// kiritilgan songa kora hafta kunlarini ifodalovchi 
+
+// let week = 3
+// switch(week){
+//     case 1:
+//         console.log("Dushanba")
+//         break
+//     case 2:
+//         console.log("Seshanba")
+//         break
+//     case 3:
+//         console.log("Chorshanba")
+//         break
+//     case 4:
+//         console.log("Payshanba")
+//         break
+//     case 5:
+//         console.log("Juma")
+//         break
+//     case 6:
+//         console.log("Shanba")
+//         break
+//     case 7:
+//         console.log("Yakshanba")
+//         break
+// }
+
+// 2-misol
+// baholash sistemasi 1 2 3 4 5 baholari agar 5 da n kata bolsa false chiqadi
+
+// let baho =5 
+// switch(baho){
+//     case 1:
+//         console.log("1 baho-yomon")
+//         break
+//     case 2:
+//         console.log("2 baho-qoniqarsiz")
+//         break
+//     case 3:
+//         console.log("3baho-qoniqarli")
+//         break
+//     case 4:
+//         console.log("4 baho-yaxshi")
+//         break
+//     case 5:
+//         console.log("5-alo")
+//         break
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//     case 10:
+//     case -1:
+//     case -2:
+//     case -3:
+//     case 0:
+//         console.log("XATO")
+// }
+
+// 3-misol
+// oy raqamini bering aniqlaberadi
+
+// let month =10
+// switch(month){
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log("Baxor")
+//         break
+//     case 4:
+//     case 5:
+//     case 6:
+//         console.log("Yoz")
+//         break
+//     case 7:
+//     case 8:
+//     case 9:
+//         console.log("Kuz")
+//         break
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log("Qish")
+//         break
+// }
+
+// 4-misol
+// kiritilgan oyda necha kun borligini aniqlidi
+
+// let monthNumber =12
+// switch(monthNumber){
+//     case 1:
+//         console.log("Yanvar-31kun")
+//         break
+//     case 2:
+//         console.log("Fevral-28kun")
+//         break
+//         case 3:
+//             console.log("Mart-31kun")
+//             break
+//         case 4:
+//             console.log("Aprel-30kun")
+//             break
+//          case 5:
+//             console.log("May-31kun")
+//             break
+//         case 6:
+//             console.log("Iyun-30kun")
+//             break
+//         case 7:
+//             console.log("Iyul-31kun")
+//             break
+//         case 8:
+//             console.log("Avgus-31kun")
+//             break
+//         case 9:
+//             console.log("Sentabr-30kun")
+//             break
+//         case 10:
+//             console.log("Aktyabr-31kun")
+//             break
+//         case 11:
+//             console.log("Nayabr-30kun")
+//             break
+//         case 12:
+//             console.log("Dekabr-31kun")
+//             break
+// }
+
+// 5-misol
+// Arifmetik amal bajaradi
+
+// let math =4
+// switch (math){
+//     case 1:
+//         // let a = +prompt ("a")
+//         // let b = +prompt ("b")
+//         // console.log( a + b )   
+
+//     case 2:
+//         // let a = +prompt ("a")
+//         // let b = +prompt ("b")
+//         // console.log( a - b )
+    
+//     case 3:
+//         // let a = +prompt ("a")
+//         // let b = +prompt ("b")
+//         // console.log( a / b )
+
+//     case 4:
+//         let a = +prompt ("a")
+//         let b = +prompt ("b")
+//         console.log( a * b )
+        
+// }
+
+// 6-misol
+// 
